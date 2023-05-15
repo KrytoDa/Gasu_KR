e46d48b122e86529f6f77284e0d5231b1529163f
Krytoo <121355650+KrytoDa@users.noreply.github.com>
2023-05-15 18:22:53 +0300

    Update CREATE .sql

--- CREATE .sql
+++ CREATE .sql
@@ -1,13 +1,12 @@
-CREATE TABLE IF NOT EXISTS categories_of_item
+CREATE TABLE IF NOT EXISTS CW_categories_of_item
 (
 categories_of_item_id INT PRIMARY KEY,
 title VARCHAR(300),
 descriprion VARCHAR(380)
 );
 
- 
 
-CREATE TABLE IF NOT EXISTS item
+CREATE TABLE IF NOT EXISTS CW_item
 (
 item_id INT PRIMARY KEY,
 title VARCHAR(300),
@@ -18,18 +17,14 @@ rating INT,
 review VARCHAR(800)
 );
 
- 
-
-CREATE TABLE IF NOT EXISTS categ_and_item
+CREATE TABLE IF NOT EXISTS CW_categ_and_item
 (
-categories_of_item_id INT REFERENCES categories_of_item(categories_of_item_id),
-item_id INT REFERENCES item(item_id),
+categories_of_item_id INT REFERENCES CW_categories_of_item(categories_of_item_id),
+item_id INT REFERENCES CW_item(item_id),
 PRIMARY KEY(categories_of_item_id, item_id)
 );
 
- 
-
-CREATE TABLE IF NOT EXISTS k_customers
+CREATE TABLE IF NOT EXISTS CW_k_customers
 (
 k_customers_id INT PRIMARY KEY,
 email VARCHAR(300) UNIQUE,
@@ -39,33 +34,27 @@ last_name VARCHAR(800),
 adress VARCHAR(800)
 );
 
- 
-
-CREATE TABLE IF NOT EXISTS shopping_cart
+CREATE TABLE IF NOT EXISTS CW_shopping_cart
 (
-k_customers_id INT REFERENCES k_customers(k_customers_id),
-item_id INT REFERENCES item(item_id),
+k_customers_id INT REFERENCES CW_k_customers(k_customers_id),
+item_id INT REFERENCES CW_item(item_id),
 PRIMARY KEY(k_customers_id, item_id),
 amount INT
 );
 
- 
-
-CREATE TABLE IF NOT EXISTS k_order
+CREATE TABLE IF NOT EXISTS CW_k_order
 (
 order_id INT PRIMARY KEY,
 status VARCHAR(300),
 payment_date DATE,
 amount INT,
-k_customers_id INT REFERENCES k_customers(k_customers_id),
-item_id INT REFERENCES item(item_id)
+k_customers_id INT REFERENCES CW_k_customers(k_customers_id),
+item_id INT REFERENCES CW_item(item_id)
 );
 
- 
-
-CREATE TABLE IF NOT EXISTS ordered_item
+CREATE TABLE IF NOT EXISTS CW_ordered_item
 (
-order_id INT REFERENCES k_order(order_id),
-item_id INT REFERENCES item(item_id),
+order_id INT REFERENCES CW_k_order(order_id),
+item_id INT REFERENCES CW_item(item_id),
 PRIMARY KEY(order_id, item_id)
 );
